74626dcd6dfa57aee598928bf8e479606488587c
Arnaud Trouvé <trouvearnaud@hotmail.com>
2014-12-16 11:39:12 +0100

    Create blender_render_settings.py

---
+++ blender_render_settings.py
@@ -0,0 +1,42 @@
+bl_info = {
+    "name": "Change Render Settings",
+    "location": "View3D > Add > Mesh > Render Settings",
+    "description": "Sets render settings of the scene",
+    "author": "Arnaud Trouve",
+    "version": (0,1),
+    "blender": (2, 7, 0),
+    "category": "Add Mesh",
+    }
+
+import bpy
+
+class renderSettings(bpy.types.Operator):
+    """Sets render settings"""
+    bl_idname = "object.render_setting"
+    bl_label = "Set Render Settings"
+    bl_options = {'REGISTER', 'UNDO'}    
+    
+    def execute(self, context):
+    
+        scene = bpy.context.scene
+        
+        scene.render.resolution_x = 2880
+        scene.render.resolution_y = 1620
+        scene.render.resolution_percentage = 100
+        scene.render.use_border = False
+        
+        return {'FINISHED'}
+
+def menu_func(self, context):
+    self.layout.operator(renderSettings.bl_idname, icon='MOD_SKIN')
+            
+def register():
+    bpy.utils.register_class(renderSettings)
+    bpy.types.INFO_MT_mesh_add.append(menu_func)
+    
+def unregister():
+    bpy.utils.unregister_class(renderSettings)
+    bpy.types.INFO_MT_mesh_add.remove(menu_func)
+    
+if __name__ == "__main__":
+    register()
