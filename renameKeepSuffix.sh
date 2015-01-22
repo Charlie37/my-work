40cb4075ace0fc64850cb98845cc568a760e9bab
Arnaud Trouvé <trouvearnaud@hotmail.com>
2015-01-22 15:29:50 +0100

    Delete renameKeepSuffix.sh

--- renameKeepSuffix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Very simple script to run through a bunch of images
-# Renames them with a same prefix while keeping their suffix (part of name after last underscore)
-for file in *.png
-do
-	STR=`echo $file | sed 's/.*\_//'` # get part of filename after last underscore
-	mv $file "prefix"$STR # rename
-done
